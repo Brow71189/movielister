4a0c7aba81ff2e49cdd9bc53e7bcafcbf8a486ec
Andreas <andreas.mittelberger@univie.ac.at>
2016-01-23 14:56:15 +0100

    Using json output of ffmpeg now. If a file was changed since it was put into the database it is updated now

--- lister.py
+++ lister.py
@@ -11,20 +11,23 @@ import time
 import subprocess
 from xml.etree import ElementTree
 import ast
+import json
 
 class Movielister(object):
 
     def __init__(self):
-        self.supported_types = ['.avi', '.mpg']
+        self.supported_types = ['.avi', '.mpg', '.wmv', '.mov', '.mkv']
         self.list_recursive = True
         self.delete_non_existant = True
+        self.always_update_all = False
         self.max_recursion_depth = -1
         self.current_recursion_depth = 0
         self.movie_list = []
         self.database_tree = None
-        self.movie_path = 'K:/Users/Andi/Videos/OTR'
-        self.database_path = 'K:/Users/Andi/Videos/OTR/movie_database.xml'
-        self.ffprobe_path = 'C:/Program Files/ffmpeg-20160116-git-d7c75a5-win64-static/bin/ffprobe.exe'
+        self.movie_path = '.'
+        self.database_path = '.'
+        self.ffprobe_path = 'avprobe'
+        self.ffprobe_options = '-of json -show_format -show_streams -loglevel quiet'
         self.metadata_elements = ['title', 'duration_minutes', 'resolution', 'language', 'date_on_tv', 'channel',
                                   'date_modified', 'file_size', 'video_codec', 'extension']
         self.htmls_path = None
@@ -79,14 +82,22 @@ class Movielister(object):
         database_movie_list = self.database_tree.getroot().findall('movie')
         for moviename in self.movie_list:
             found = False
+            counter = 0
             for movie in database_movie_list:
                 if os.path.basename(moviename) == movie.get('filename'):
                     found = True
                     break
+                counter += 1
             
             if not found:
                 print('Creating new entry for ' + moviename)
                 self.database_tree.getroot().append(self.create_movie_node(moviename))
+            else:
+                now_last_updated = time.strftime('%Y_%m_%d_%H_%M', time.localtime(os.path.getmtime(moviename)))
+                last_updated = database_movie_list[counter].get('date_modified')
+                if self.always_udate_all or (last_updated is not None and now_last_updated > last_updated.text):
+                    print('Updating entry for ' + moviename)
+                    self.update_movie_node(database_movie_list[counter])
         
         self.database_tree.getroot().set('last_updated', time.strftime('%Y_%m_%d_%H_%M'))
         self.database_tree.getroot().set('metadata_elements', str(self.metadata_elements))
@@ -101,7 +112,17 @@ class Movielister(object):
                 movie_property.text = str(value)
                 movie_node.append(movie_property)
         
-        return movie_node       
+        return movie_node
+        
+    def update_movie_node(self, movie_node):
+        metadata_dict = self.get_movie_metadata(movie_node.get('filename'))
+        
+        for key, value in metadata_dict.items():
+            movie_property = movie_node.find('key')
+            if key in self.metadata_elements and movie_property is not None:
+                movie_property.text = str(value)
+            elif movie_property is not None:
+                movie_node.remove(movie_property)
     
     def get_movielist(self, movie_path=None, current_recursion_depth=0):
         self.movie_path = os.path.normpath(self.movie_path)
@@ -132,38 +153,74 @@ class Movielister(object):
         self.ffprobe_path = os.path.normpath(self.ffprobe_path)
         ffmpeg_metadata = {}
         try:
-            ffmpeg_out = subprocess.check_output([self.ffprobe_path, filename], stderr=subprocess.STDOUT,
-                                                 universal_newlines=True)
+#            ffmpeg_out = subprocess.check_output([self.ffprobe_path, filename], stderr=subprocess.STDOUT,
+#                                                 universal_newlines=True)
+            args_list = []
+            args_list.append(self.ffprobe_path)
+            args_list.extend(self.ffprobe_options.split())
+            args_list.append(filename)
+            ffmpeg_out = subprocess.check_output(args_list)
+            ffmpeg_out = json.loads(ffmpeg_out)
         except Exception as detail:
             print('Could not get ffmpeg metadata for ' + filename + '. Reason: ' + str(detail))
             return ffmpeg_metadata
         
-        ffmpeg_out = ffmpeg_out.split('\n')
+        #ffmpeg_out = ffmpeg_out.split('\n')
         
-        for element in ffmpeg_out:
-            element = element.strip().lower()
-            if element.startswith('duration'):
-                try:
-                    element = element.split(', ')[0]
-                    element = element.split(': ')[1]
-                    element = element.split(':')
-                    element = 60*int(element[0]) + int(element[1]) + (0 if float(element[2]) < 30 else 1)
-                    ffmpeg_metadata['duration_minutes'] = str(element)
-                except Exception as detail:
-                    print('Could not extract video duration from ' + filename + '. Reason: ' + str(detail))
-            elif element.startswith('stream #0') and element.find('video:') > -1:
-                try:
-                    element = element.split(', ')
-                    codec = element[0].split('video:')[1].strip()
-                    codec = codec.split()[0]
-                    resolution = element[2].strip().split()[0] if ((element[2].find('sar') > -1 or
-								    element[2].find('par') > -1)
-								    and element[2].find('dar') > -1) else (
-                                                                                          element[3].strip().split()[0])
-                    ffmpeg_metadata['video_codec'] = codec
-                    ffmpeg_metadata['resolution'] = resolution
-                except Exception as detail:
-                    print('Could not extract video codec and resolution from ' + filename + '. Reason: ' + str(detail))
+        try:
+            duration = float(ffmpeg_out['format']['duration'])
+            duration = int(duration/60) + (0 if duration%60 < 30 else 1)
+            ffmpeg_metadata['duration_minutes'] = str(duration)
+        except Exception as detail:
+            print('Could not extract video duration from ' + filename + '. Reason: ' + str(detail))
+        
+        video_stream = None
+        try:
+            for i in range(len(ffmpeg_out['streams'])):
+                if ffmpeg_out['streams'][i]['codec_type'] == 'video':
+                    video_stream = ffmpeg_out['streams'][i]
+                    break
+            else:
+                raise Exception('No video stream found in list of streams.')
+                
+        except Exception as detail:
+            print('Could not find any video streams in ' + filename + '. Reason: ' + str(detail))
+            return ffmpeg_metadata
+        
+        try:
+            ffmpeg_metadata['video_codec'] = video_stream['codec_name']
+        except Exception as detail:
+            print('Could not extract video codec info from ' + filename + '. Reason: ' + str(detail))
+            
+        try:
+            ffmpeg_metadata['resolution'] = video_stream['width'] + 'x' + video_stream['height']
+        except Exception as detail:
+            print('Could not extract video resolution from ' + filename + '. Reason: ' + str(detail))
+            
+#        for element in ffmpeg_out:
+#            element = element.strip().lower()
+#            if element.startswith('duration'):
+#                try:
+#                    element = element.split(', ')[0]
+#                    element = element.split(': ')[1]
+#                    element = element.split(':')
+#                    element = 60*int(element[0]) + int(element[1]) + (0 if float(element[2]) < 30 else 1)
+#                    ffmpeg_metadata['duration_minutes'] = str(element)
+#                except Exception as detail:
+#                    print('Could not extract video duration from ' + filename + '. Reason: ' + str(detail))
+#            elif element.startswith('stream #0') and element.find('video:') > -1:
+#                try:
+#                    element = element.split(', ')
+#                    codec = element[0].split('video:')[1].strip()
+#                    codec = codec.split()[0]
+#                    resolution = element[2].strip().split()[0] if ((element[2].find('sar') > -1 or
+#								    element[2].find('par') > -1)
+#								    and element[2].find('dar') > -1) else (
+#                                                                                          element[3].strip().split()[0])
+#                    ffmpeg_metadata['video_codec'] = codec
+#                    ffmpeg_metadata['resolution'] = resolution
+#                except Exception as detail:
+#                    print('Could not extract video codec and resolution from ' + filename + '. Reason: ' + str(detail))
 
         return ffmpeg_metadata
         
